a294bc7793fee042af498a4feef08df0afda8a96
Ivan Coronado Moreno <ivacormor@gmail.com>
2022-08-03 12:04:55 +0200

    chore: demo without dependencies

---
+++ apps/web/pages/test/[slug].tsx
@@ -0,0 +1,33 @@
+import { ParsedUrlQuery } from "querystring";
+
+import { GetStaticPaths, GetStaticProps } from "next";
+
+interface Params extends ParsedUrlQuery {
+  slug: string;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return { paths: [], fallback: true };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as Params;
+  const demo = `Hello ${slug}`;
+  console.log(demo);
+
+  return {
+    props: {
+      demo,
+    },
+    revalidate: 20,
+  };
+};
+
+export default function Web(props: any) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Web {props.demo}</h1>
+    </div>
+  );
+}
